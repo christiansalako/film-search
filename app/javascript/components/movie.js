050910c9c43cfda7baf530d59357b587cd3ce876
christian salako <christian.s5547@gmail.com>
2020-04-06 13:30:16 +0100

    pushed to heroku

---
+++ app/javascript/components/movie.js
@@ -0,0 +1,27 @@
+const results = document.querySelector("#results");
+
+
+  const searchMovies = (word) => {
+    fetch(`http://www.omdbapi.com/?s=${word}&apikey=adf1f2d7`)
+      .then(response => response.json())
+      .then((data) => {
+        data.Search.forEach((movie) => {
+          const newMovie = `<li>
+            <img src="${movie.Poster}" height="200" />
+          </li>`
+          results.insertAdjacentHTML('beforeend', newMovie)
+        });
+      });
+  }
+
+const searchForm = document.querySelector("#search-movies")
+
+
+    searchForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      results.innerHTML = "";
+      const word = document.querySelector("#keyword").value;
+      searchMovies(word);
+    });
+
+export { searchMovies };
